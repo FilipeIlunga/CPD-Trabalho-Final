2ab529cb4e441de63052d3d365c2012f2be1f1eb
Filipe Ilunga <filipeilunga@gmail.com>
2021-05-16 20:59:23 -0300

    Menu interativo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import os
 from csvArq import *
+#--coding: ANSI - -
 
 # Funções
 # ==================================================================================================================================
@@ -9,60 +10,66 @@ def switch(menu, handler):
     #implementação de menu com iteração caso usuário faça escolhas erradas.
     while menu != 0:
         if menu == 1:
-            candidato(handler, input("Nome procurado: "), int(input("Qual o máximo de candidatos deseja listar?: ")))
+            candidato(handler, input("Nome procurado: "), int(input("Qual o máximo de candidatos deseja listar?: ")),menu)
             break
         elif menu == 2:
-            ocupacao(handler, input("Profissão: "), int(input("Qual o máximo de candidatos deseja listar?: ")))
+            ocupacao(handler, input("Profissão: "), int(input("Qual o máximo de candidatos deseja listar?: ")),menu)
             break
         elif menu == 3:
-            cargo(handler, input("Cargo procurado: "), int(input("Qual o máximo de candidatos deseja listar?: ")))
+            cargo(handler, input("Cargo procurado: "), int(input("Qual o máximo de candidatos deseja listar?: ")),menu)
+            break
+        else:
+            print('[%s] É COMANDO INVALIDO, TENTE NOVAMENTE\n' %  menu)
             break
     if menu == 0:
         print("###### PROGRAMA FINALIZADO! ######\n")
            
     return 0
 
+def printMenu(menu):
+    if menu == 3:
+        print("\n\nORDENAR POR:")
+        print("IDADE\n")
+        print("\t[1]  Decrescente\n")
+        print("\t[2]  Crescente.\n")
+        print("GRAU DE INSTRUÇÃO.\n")
+        print("\t[5]  Decrescente\n")
+        print("\t[6]  Crescente.\n")
+    else:
+        print("\n\nORDENAR POR:")
+        print("IDADE\n")
+        print("\t[1]  Decrescente\n")
+        print("\t[2]  Crescente.\n")
+        print("CARGO DISPUTADO.\n")
+        print("\t[3]  Decrescente\n")
+        print("\t[4]  Crescente.\n")
+        print("GRAU DE INSTRUÇÃO.\n")
+        print("\t[5]  Decrescente\n")
+        print("\t[6]  Crescente.\n")
+        print("\n\t[0] BUSCAR OUTRO CANDIDATO")
 
 
-def candidato(name_archive, nomeCandidato, numMaxCandidatos):
-    print("\n\nORDENACAO:")
-    print("#### Ordenar por Idade ####.\n")
-    print("\t[1]  maior para o menor.\n")
-    print("\t[2]  Menor para o Maior.\n")
-    print("#### Ordenar por Cargo ####.\n")
-    print("\t[3]  maior para o menor.\n")
-    print("\t[4]  Menor para o Maior.\n")
-    print("#### Ordenar por grau de instrucao ####.\n")
-    print("\t[5]  maior para o menor.\n")
-    print("\t[6]  Menor para o Maior.\n")
+def candidato(name_archive, nomeCandidato, numMaxCandidatos,menuPrint):
     
-    menu = int(input())
+    menu=1
+    while menu!= 0:
+        printMenu(menuPrint)
+        menu = int(input())
+        if menu == 0:
+            break
+        #função lê o arquivo e retorna os dados do candidato
+        list_cand = []
+        f = open(name_archive + "data_candidato_index.bin", 'rb')
+        tree = pickle.load(f)
+        nomeCandidato = nomeCandidato.lower()
+        list_cand = list_candidato(nomeCandidato, tree, name_archive)
+        print_list(list_cand, numMaxCandidatos,menu)
+        f.close()
 
-    #função lê o arquivo e retorna os dados do candidato
-    list_cand = []
 
-    f = open(name_archive + "data_candidato_index.bin", 'rb')
+def ocupacao(name_archive, ocupacaoCandidato, numMaxCandidatos,menuPrint):
 
-    tree = pickle.load(f)
-    nomeCandidato = nomeCandidato.lower()
-    list_cand = list_candidato(nomeCandidato, tree, name_archive)
-
-    print_list(list_cand, numMaxCandidatos,menu)
-
-    f.close()
-
-
-def ocupacao(name_archive, ocupacaoCandidato, numMaxCandidatos):
-    print("\n\nORDENACAO:")
-    print("#### Ordenar por Idade ####.\n")
-    print("\t[1]  maior para o menor.\n")
-    print("\t[2]  Menor para o Maior.\n")
-    print("#### Ordenar por Cargo ####.\n")
-    print("\t[3]  maior para o menor.\n")
-    print("\t[4]  Menor para o Maior.\n")
-    print("#### Ordenar por grau de instrucao ####.\n")
-    print("\t[5]  maior para o menor.\n")
-    print("\t[6]  Menor para o Maior.\n")
+    printMenu(menuPrint)
     
     menu = int(input())
     #função lê o arquivo e retorna os dados do candidato da ocupação informada
@@ -81,14 +88,9 @@ def ocupacao(name_archive, ocupacaoCandidato, numMaxCandidatos):
     f.close()
 
 
-def cargo(name_archive, cargoCandidato, numMaxCandidatos):
-    print("\n\nORDENACAO:")
-    print("#### Ordenar por Idade ####.\n")
-    print("\t[1]  maior para o menor.\n")
-    print("\t[2]  Menor para o Maior.\n")
-    print("#### Ordenar por grau de instrucao ####.\n")
-    print("\t[5]  maior para o menor.\n")
-    print("\t[6]  Menor para o Maior.\n")
+def cargo(name_archive, cargoCandidato, numMaxCandidatos,menuPrint):
+
+    printMenu(menuPrint)
     
     menu = int(input())
     #função lê o arquivo e retorna os dados do candidato que disputou o cargo informado
@@ -154,11 +156,13 @@ print("\nArquivo " + handler + " aberto e processado com sucesso\n")
 
 menu = 1
 while menu != 0:
-    print("Escolha uma das opções abaixo:\n\n")
+    print("\n___________________________________\n")
+    print("| ESCOLHA UMA DAS OPÇÕES ABAIXO   |")
+    print("___________________________________\n")
     print("[1] Buscar Candidato.\n")
     print("[2] Listagem de candidatos por ocupação.\n")
     print("[3] Listagem de cargos disputados.\n")
     print("[0] Finalizar programa.\n")
     menu = int(input())
     switch(menu, handler)
-    input()
+
